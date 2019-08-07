4a1942babc061c72eb54bf76dd96f6614822b929
Kaustav96 <kaustavbanerjee96@gmail.com>
2019-08-07 22:48:17 +0530

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messagetwo = document.querySelector('#message-2');
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();//prevents default behaviour and stops browser from refreshing
+
+    const location = search.value;
+    messageOne.textContent = 'Loading...';
+    messagetwo.textContent = '';
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent="Location is - "+data.location;
+                messagetwo.textContent="Forecast is - "+data.forecast;
+                console.log("Location given - " + data.location)
+                console.log("Forecast given - " +data.forecast)
+            }
+        })
+    })
+})
+
